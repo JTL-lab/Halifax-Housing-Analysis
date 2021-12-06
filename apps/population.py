bcc1466bd347c5781da07843c4b80fcc82b836b0
Grant Sutherland <65304335+GSutherland36@users.noreply.github.com>
2021-12-06 07:13:05 -0400

    Updated the population page with more visualizations and writeups

--- apps/population.py
+++ apps/population.py
@@ -20,6 +20,18 @@ hfx_census = preprocessing.return_dataframe()
 hfx_json = preprocessing.return_geojson()
 census_cols = list(hfx_census.columns)
 
+tract_data_url2006 = 'https://raw.githubusercontent.com/JTL-lab/Halifax-Housing-Analysis/main/data/hfxTractages2006.csv'
+tractAges2006 = pd.read_csv(tract_data_url2006, index_col=0)
+pop_2006 = tractAges2006.reset_index(drop = True)
+
+tract_data_url2011 = 'https://raw.githubusercontent.com/JTL-lab/Halifax-Housing-Analysis/main/data/hfxTractages2011.csv'
+tractAges2011 = pd.read_csv(tract_data_url2011, index_col=0)
+pop_2011 = tractAges2011.reset_index(drop = True)
+
+tract_data_url2016 = 'https://raw.githubusercontent.com/JTL-lab/Halifax-Housing-Analysis/main/data/hfxTractages2016.csv'
+tractAges2016 = pd.read_csv(tract_data_url2016, index_col=0)
+pop_2016 = tractAges2016.reset_index(drop = True)
+
 def find_growth_without_childbirth():
     """
     Calculates the average growth without childbirth factored in per age demographic in Halifax between census years.
@@ -38,6 +50,86 @@ def find_growth_without_childbirth():
         hfx_census[col_name] = ((hfx_census['Population'+year] - hfx_census['BornSince'+year_before] -
                                 hfx_census['Population'+year_before]) / hfx_census['Population'+year_before]) * 100
 
+def getAgeGroupTotals(dfPop):
+    total1519 = 0
+    total2024 = 0
+    total2529 = 0
+    total3034 = 0
+    total3539 = 0
+    total4044 = 0
+    total4549 = 0
+    total5054 = 0
+    total5559 = 0
+    total6064 = 0
+    for index, row in dfPop.iterrows():
+        total1519 += row['15-19']
+        total2024 += row['20-24']
+        total2529 += row['25-29']
+        total3034 += row['30-34']
+        total3539 += row['35-39']
+        total4044 += row['40-44']
+        total4549 += row['45-49']
+        total5054 += row['50-54']
+        total5559 += row['55-59']
+        total6064 += row['60-64']
+
+    ageGroupDict = {'15-19': total1519, '20-24': total2024, '25-29': total2529, '30-34': total3034, '35-39': total3539, '40-44': total4044, '45-49': total4549, '50-54': total5054, '55-59': total5559, '60-64': total6064}
+    return ageGroupDict
+
+ageGroupPops2006 = getAgeGroupTotals(pop_2006)
+ageGroupPops2011 = getAgeGroupTotals(pop_2011)
+ageGroupPops2016 = getAgeGroupTotals(pop_2016)
+
+    # first_census and second_census are 2 out of the three dictionaries (ageGroupPops2006, ageGroupPops2011, ageGroupPops2016)
+    # this function follows a specific group of people over five years to see how their demographic grew
+    # for example, it will measure growth by comparing thr 15-19 year old population in 2006 to the 20-24 year old population in 2011
+    # this tells us specifically what age people are when they are come to and/or leave Halifax
+def measureDemographicGrowth(firstCensus, secondCensus):
+    ageRanges = ['15-19', '20-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64']
+
+    growthOfRanges = []
+
+    for i in range(len(ageRanges) - 1):
+        popFirstCensus = firstCensus[ageRanges[i]]
+        popSecondCensus = secondCensus[ageRanges[i + 1]]
+
+        growthToSecondCensus = (popSecondCensus - popFirstCensus) / popFirstCensus * 100
+        growthOfRanges.append(growthToSecondCensus)
+
+    return growthOfRanges
+
+def getAgeBarGraph20112016():
+    # growth of age demographics from 2011 to 2016
+    growthOfAgeDemographics = measureDemographicGrowth(ageGroupPops2011, ageGroupPops2016)
+
+    lowerAges = [15, 20, 25, 30, 35, 40, 45, 50, 55]
+    birthYearRanges = []
+
+    for i in lowerAges:
+      firstBirthYear = 2011 - i
+      secondBirthYear = 2011 - (i + 4)
+      birthYearRanges.append(str(firstBirthYear) + '-' + str(secondBirthYear))
+
+    fig = px.bar(x=birthYearRanges, y=growthOfAgeDemographics)
+    fig.update_layout(title = 'Population Growth of Age Demographics 2011-2016', xaxis_title = 'Year of Birth', yaxis_title = 'Population Growth Percentage 2011-2016')
+    return fig
+
+def getAgeBarGraph20062011():
+    # growth of age demographics from 2011 to 2016
+
+    growthOfAgeDemographics = measureDemographicGrowth(ageGroupPops2006, ageGroupPops2011)
+
+    lowerAges = [15, 20, 25, 30, 35, 40, 45, 50, 55]
+    birthYearRanges = []
+
+    for i in lowerAges:
+      firstBirthYear = 2006 - i
+      secondBirthYear = 2006 - (i + 4)
+      birthYearRanges.append(str(firstBirthYear) + '-' + str(secondBirthYear))
+
+    fig = px.bar(x=birthYearRanges, y=growthOfAgeDemographics)
+    fig.update_layout(title = 'Population Growth of Age Demographics 2006-2011', xaxis_title = 'Year of Birth', yaxis_title = 'Population Growth Percentage 2006-2011')
+    return fig
 
 # Ensure that population growth without childbirth cols are added to dataframe
 find_growth_without_childbirth()
@@ -73,6 +165,12 @@ layout = html.Div([
 
         dcc.Graph(id="density_choropleth"),
 
+        dbc.Row([
+            dbc.Col([
+                html.P("The above visualization shows the population per square kilometre in each tract.")
+            ])
+        ]),
+
         # Section 2: Population Change
         dbc.Row([
             dbc.Col(dbc.Card(html.H3(children='Population Change Between Census Years',
@@ -85,15 +183,21 @@ layout = html.Div([
         dcc.Dropdown(
             id='pop_change_year',
             options=[
-                {'label': '2001-2006', 'value': 'PopulationDensity2006'},
-                {'label': '2006-2011', 'value': 'PopulationDensity2011'},
-                {'label': '2011-2016', 'value': 'PopulationDensity2016'}
+                {'label': '2001-2006', 'value': 'PopulationChange2001-2006'},
+                {'label': '2006-2011', 'value': 'PopulationChange2006-2011'},
+                {'label': '2011-2016', 'value': 'PopulationChange2011-2016'}
             ],
-            value='PopulationDensity2006',
+            value='PopulationChange2001-2006',
             style={'width': '50%', 'margin-left': '5px'}
         ),
 
         dcc.Graph(id="pop_change_choropleth"),
+
+        dbc.Row([
+            dbc.Col([
+                html.P("The above visualization shows the population growth from one census to the next as a percentage for each tract.")
+            ])
+        ]),
 
         # Section 3: Population Growth and Shrinkage Without Childbirths
         dbc.Row([
@@ -115,8 +219,59 @@ layout = html.Div([
             style={'width': '50%', 'margin-left': '5px'}
         ),
 
-        dcc.Graph(id="no_childbirths_choropleth")
+        dcc.Graph(id="no_childbirths_choropleth"),
 
+        dbc.Row([
+            dbc.Col([
+                html.P("The above visualization shows the percentage of population growth that each tract would have experienced " 
+                       "if no children had been born since the last census. If the growth is positive, it means that "
+                       "since the last census, the number of people who moved in is greater than the number of people "
+                       "who moved away or died. If the the growth is negative, there were more people who died or moved away "
+                       "than people who moved in. We calculated that from 2001-2006, the growth of Metro Halifax without child birth was -3.97%. "
+                       "The total growth was 0.999%. From 2006-2011 the growth of Metro Halifax without child birth was -0.094%. The total "
+                       "growth was 1.04%. From 2011-2016 the growth of Metro Halifax without child birth was -1.04%. The total growth was 1.03%. "
+                       "Given that Canada has a steadily declining birthrate which is one of among the lowest in the world [1], the current population trends "
+                       "are likely not sustainable for the long term future as Halifax still depends on child birth for this small amount "
+                       "of growth which keeps the population growing.")
+            ])
+        ]),
+
+        # Dropdown menu for age group growth bar graph
+        html.P("Census Years:"),
+        dcc.Dropdown(
+            id='age_bar_graph_year',
+            options=[
+                {'label': '2006-2011', 'value': 2006},
+                {'label': '2011-2016', 'value': 2011},
+            ],
+            value=2006,
+            style={'width': '50%', 'margin-left': '5px'}
+        ),
+
+        dcc.Graph(id="age_bar_graph"),
+
+        dbc.Row([
+            dbc.Col([
+                html.P("The above bar graph shows the percentage growth of each individual age group from one census to the next for all of Metro Halifax. "
+                       "For example, growth is measured by comparing the population of 15-19 year olds in 2006, to the population of 20-24 year olds in 2011. "
+                       "This is the population growth of a specific age demographic ageing five years from one census to the next. "
+                       "As we can see from the graph, the population of 15-19 year olds more than doubles by the time they are in the 20-24 year old age group. "
+                       "This is the only age demographic that saw significant growth from 2006-2016. "
+                       "The most obvious explanation for this repeated increase is the fact that Halifax is home to several post-secondary "
+                       "institutions. The reason behind the lack of growth in every other age demographic is less obvious. "
+                       "However, it is clear that more work needs to be done to keep and attract more people outside of the typical post-secondary age group.")
+            ])
+        ]),
+
+        dbc.Row([
+            dbc.Row([
+                dbc.Col(html.H5("REFERENCES"), className="mb-5 mt-5")
+
+            ]),
+            dbc.Row([
+                html.P("[1] “Fertility rate, total (births per woman) - canada,” Data. [Online]. Available: https://data.worldbank.org/indicator/SP.DYN.TFRT.IN?locations=CA. [Accessed: 06-Dec-2021]. ")
+            ])
+        ])
     ])
 ])
 
@@ -180,3 +335,13 @@ def display_population_change_choropleth(no_childbirths_year):
     )
 
     return no_childbirths_choropleth
+
+@app.callback(
+    Output("age_bar_graph", "figure"),
+    [Input("age_bar_graph_year", "value")]
+)
+def display_population_change_choropleth(age_bar_graph):
+    if age_bar_graph == 2006:
+        return getAgeBarGraph20062011()
+    elif age_bar_graph == 2011:
+        return getAgeBarGraph20112016()
