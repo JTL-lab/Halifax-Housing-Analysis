0759e2f506b65c962c54360c208da842b6d74557
Julia <68295300+JTL-lab@users.noreply.github.com>
2022-01-19 18:42:00 -0400

    Update index.py

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 
 from app import app
+from app import server
 from apps import home, population, gentrification, housing, recommendations
 
 # Navigation bar!
